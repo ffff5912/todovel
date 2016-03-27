f6fce8ec73086677b376a3255d5a5134cd239f5b
ffff5912 <iioka5912@gmail.com>
2016-03-27 21:37:15 +0900

    add section block

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,6 +1,13 @@
+<!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <title>App Name - @yield('title')</title>
+        @section('stylesheets')
+            <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+            <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+        @show
     </head>
     <body>
         @if (Session::has('message'))
@@ -18,5 +25,7 @@
         <div class="container">
             @yield('content')
         </div>
+        @section('javascripts')
+        @show
     </body>
 </html>
