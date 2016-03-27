0f1323106e004a09368abe41cfcc2e0cab2c6b8b
ffff5912 <iioka5912@gmail.com>
2016-03-27 21:09:47 +0900

    add action

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -2,11 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use Input;
+use Redirect;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use App\Project;
+use App\Http\Controllers;
 
 class ProjectsController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::all();
+        return view('projects.index', compact('projects'));
+    }
+
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    public function store()
+    {
+        $input = Input::all();
+        Project::create($input);
+
+        return Redirect::route('projects.index')->with('message', 'Project created');
+    }
+
+    public function show(Project $project)
+    {
+        return view('projects.show', compact('project'));
+    }
+
+    public function edit(Project $project)
+    {
+        return view('projects.show', compact('project'));
+    }
+
+    public function update(Project $project)
+    {
+        $input = array_except(Input::all(), '_method');
+        $project->update($input);
+
+        return Redirect::route('projects.show', $project->slug)->with('message', 'Project updated');
+    }
+
+    public function destoroy(Project $project)
+    {
+        $project->delete();
+
+        return Redirect::route('projects.index')->with('message', 'Project deleted');
+    }
 }
