f7ced2b7df097a2156443980fbc50c3f768f00ef
ffff5912 <iioka5912@gmail.com>
2016-04-09 00:02:42 +0900

    fix

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h2>Create Project</h2>
 
-    {!! Form::model(new App\Project, ['route' => ['projects.store']]) !!}
+    {!! Form::model(new App\Models\Project, ['route' => ['projects.store']]) !!}
         @include('projects/partials/_form', ['submit_text' => 'Create Project'])
     {!! Form::close() !!}
 @endsection
