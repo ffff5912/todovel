7406fd5e87d2d45949ed8732cad7bcf13a05999a
ffff5912 <iioka5912@gmail.com>
2016-04-09 00:02:33 +0900

    create model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -12,4 +12,9 @@ class Project extends Model
     {
         return $this->hasMany('App\Models\Task');
     }
+
+    public function photos()
+    {
+        return $this->hasMany('App\Models\Photo');
+    }
 }
---
+++ database/migrations/2016_04_08_145407_create_photos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id')->unsigned()->default(0);
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->string('name')->default('');
+            $table->string('path')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('photos');
+    }
+}
